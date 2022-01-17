0e69cc580c6298ae700e354dcc74763d95ca9a63
Scott Holmes <scott.holmes44@gmail.com>
2022-01-17 16:16:44 -0700

    First commit (setup Suppliers form)

--- Workshop 4/Group_1_Travel_Experts/frmSuppliers.Designer.cs
+++ Workshop 4/Group_1_Travel_Experts/frmSuppliers.Designer.cs
@@ -30,7 +30,149 @@ namespace Group_1_Travel_Experts
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSuppliers));
+            this.dataGridViewSuppliers = new System.Windows.Forms.DataGridView();
+            this.labelSuppliers = new System.Windows.Forms.Label();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonModify = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonModifyProduct = new System.Windows.Forms.Button();
+            this.buttonRemoveContact = new System.Windows.Forms.Button();
+            this.buttonAddContact = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelSupplierContacts = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSuppliers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridViewSuppliers
+            // 
+            this.dataGridViewSuppliers.BackgroundColor = System.Drawing.Color.Linen;
+            this.dataGridViewSuppliers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSuppliers.Location = new System.Drawing.Point(44, 35);
+            this.dataGridViewSuppliers.Name = "dataGridViewSuppliers";
+            this.dataGridViewSuppliers.RowTemplate.Height = 25;
+            this.dataGridViewSuppliers.Size = new System.Drawing.Size(863, 296);
+            this.dataGridViewSuppliers.TabIndex = 0;
+            // 
+            // labelSuppliers
+            // 
+            this.labelSuppliers.AutoSize = true;
+            this.labelSuppliers.BackColor = System.Drawing.Color.Transparent;
+            this.labelSuppliers.Location = new System.Drawing.Point(44, 11);
+            this.labelSuppliers.Name = "labelSuppliers";
+            this.labelSuppliers.Size = new System.Drawing.Size(228, 21);
+            this.labelSuppliers.TabIndex = 1;
+            this.labelSuppliers.Text = "Suppliers (select by clicking)";
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonRemove.BackColor = System.Drawing.Color.BurlyWood;
+            this.buttonRemove.ForeColor = System.Drawing.Color.Maroon;
+            this.buttonRemove.Location = new System.Drawing.Point(618, 349);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(154, 39);
+            this.buttonRemove.TabIndex = 9;
+            this.buttonRemove.Text = "&Remove Supplier";
+            this.buttonRemove.UseVisualStyleBackColor = false;
+            // 
+            // buttonModify
+            // 
+            this.buttonModify.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonModify.BackColor = System.Drawing.Color.BurlyWood;
+            this.buttonModify.ForeColor = System.Drawing.Color.Maroon;
+            this.buttonModify.Location = new System.Drawing.Point(341, 348);
+            this.buttonModify.Name = "buttonModify";
+            this.buttonModify.Size = new System.Drawing.Size(166, 39);
+            this.buttonModify.TabIndex = 8;
+            this.buttonModify.Text = "&Modify Supplier";
+            this.buttonModify.UseVisualStyleBackColor = false;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonAdd.BackColor = System.Drawing.Color.BurlyWood;
+            this.buttonAdd.ForeColor = System.Drawing.Color.Maroon;
+            this.buttonAdd.Location = new System.Drawing.Point(84, 349);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(139, 37);
+            this.buttonAdd.TabIndex = 7;
+            this.buttonAdd.Text = "&Add Supplier";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExit.BackColor = System.Drawing.Color.BurlyWood;
+            this.buttonExit.ForeColor = System.Drawing.Color.Maroon;
+            this.buttonExit.Location = new System.Drawing.Point(811, 624);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(85, 39);
+            this.buttonExit.TabIndex = 10;
+            this.buttonExit.Text = "&Exit";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonModifyProduct
+            // 
+            this.buttonModifyProduct.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonModifyProduct.BackColor = System.Drawing.Color.BurlyWood;
+            this.buttonModifyProduct.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonModifyProduct.ForeColor = System.Drawing.Color.Maroon;
+            this.buttonModifyProduct.Location = new System.Drawing.Point(771, 553);
+            this.buttonModifyProduct.Name = "buttonModifyProduct";
+            this.buttonModifyProduct.Size = new System.Drawing.Size(136, 37);
+            this.buttonModifyProduct.TabIndex = 16;
+            this.buttonModifyProduct.Text = "Modify Contact";
+            this.buttonModifyProduct.UseVisualStyleBackColor = false;
+            // 
+            // buttonRemoveContact
+            // 
+            this.buttonRemoveContact.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonRemoveContact.BackColor = System.Drawing.Color.BurlyWood;
+            this.buttonRemoveContact.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonRemoveContact.ForeColor = System.Drawing.Color.Maroon;
+            this.buttonRemoveContact.Location = new System.Drawing.Point(771, 493);
+            this.buttonRemoveContact.Name = "buttonRemoveContact";
+            this.buttonRemoveContact.Size = new System.Drawing.Size(136, 41);
+            this.buttonRemoveContact.TabIndex = 15;
+            this.buttonRemoveContact.Text = "Remove Contact";
+            this.buttonRemoveContact.UseVisualStyleBackColor = false;
+            // 
+            // buttonAddContact
+            // 
+            this.buttonAddContact.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonAddContact.BackColor = System.Drawing.Color.BurlyWood;
+            this.buttonAddContact.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonAddContact.ForeColor = System.Drawing.Color.Maroon;
+            this.buttonAddContact.Location = new System.Drawing.Point(771, 437);
+            this.buttonAddContact.Name = "buttonAddContact";
+            this.buttonAddContact.Size = new System.Drawing.Size(136, 37);
+            this.buttonAddContact.TabIndex = 14;
+            this.buttonAddContact.Text = "Add Contact";
+            this.buttonAddContact.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Linen;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(84, 437);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(660, 226);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // labelSupplierContacts
+            // 
+            this.labelSupplierContacts.AutoSize = true;
+            this.labelSupplierContacts.BackColor = System.Drawing.Color.Transparent;
+            this.labelSupplierContacts.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelSupplierContacts.Location = new System.Drawing.Point(84, 413);
+            this.labelSupplierContacts.Name = "labelSupplierContacts";
+            this.labelSupplierContacts.Size = new System.Drawing.Size(250, 21);
+            this.labelSupplierContacts.TabIndex = 19;
+            this.labelSupplierContacts.Text = "Contacts in the selected Supplier";
             // 
             // frmSuppliers
             // 
@@ -38,7 +180,18 @@ namespace Group_1_Travel_Experts
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Group_1_Travel_Experts.Properties.Resources.MyBackground_;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(516, 428);
+            this.ClientSize = new System.Drawing.Size(945, 685);
+            this.Controls.Add(this.labelSupplierContacts);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonModifyProduct);
+            this.Controls.Add(this.buttonRemoveContact);
+            this.Controls.Add(this.buttonAddContact);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonModify);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.labelSuppliers);
+            this.Controls.Add(this.dataGridViewSuppliers);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.Color.Maroon;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -46,10 +199,26 @@ namespace Group_1_Travel_Experts
             this.Name = "frmSuppliers";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Suppliers";
+            this.Load += new System.EventHandler(this.frmSuppliers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSuppliers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewSuppliers;
+        private System.Windows.Forms.Label labelSuppliers;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonModify;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonModifyProduct;
+        private System.Windows.Forms.Button buttonRemoveContact;
+        private System.Windows.Forms.Button buttonAddContact;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelSupplierContacts;
     }
 }
--- Workshop 4/Group_1_Travel_Experts/frmSuppliers.cs
+++ Workshop 4/Group_1_Travel_Experts/frmSuppliers.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Group_1_Travel_Experts.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
@@ -13,6 +15,32 @@ namespace Group_1_Travel_Experts
         public frmSuppliers()
         {
             InitializeComponent();
+        }
+
+        private void frmSuppliers_Load(object sender, EventArgs e)
+        {
+            RefreshDataGridViewSuppliers(); // displays the newest Supplier data
+        }
+
+        public void RefreshDataGridViewSuppliers()
+        {
+            dataGridViewSuppliers.DataSource = null; // setting DataSource as null helps to clear the listbox in the next line
+            dataGridViewSuppliers.Rows.Clear(); // clearing the DataGridViewSuppliers
+
+            using (TravelExpertsContext db = new TravelExpertsContext()) // creating a new DBContext instance
+            {
+                dataGridViewSuppliers.DataSource = db.Suppliers.ToList(); // displays the data from the Suppliers table in the data grid
+
+                // Rename the columns
+                dataGridViewSuppliers.Columns["SupplierId"].HeaderText = "Supplier ID";
+                dataGridViewSuppliers.Columns["SupName"].HeaderText = "Supplier Name";
+
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close(); // closes this form but allows the application to continue running
         }
     }
 }
