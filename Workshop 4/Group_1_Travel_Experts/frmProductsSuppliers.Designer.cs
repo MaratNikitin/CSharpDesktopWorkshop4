4eb7c30fdb2829da89f30dae95b383312951e380
JackyLuong <jackyluong726@gmail.com>
2022-01-18 08:00:43 -0700

    Display products and it's suppliers
    - Created a combo box for all the products.
    - Each products has a list of suppliers that supply said product.
    - Currently, the products list is organized by product id in order to find the corresponding suppliers.

--- Workshop 4/Group_1_Travel_Experts/frmProductsSuppliers.Designer.cs
+++ Workshop 4/Group_1_Travel_Experts/frmProductsSuppliers.Designer.cs
@@ -30,7 +30,37 @@ namespace Group_1_Travel_Experts
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmProductsSuppliers));
+            this.cboProducts = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstSuppliers = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // cboProducts
+            // 
+            this.cboProducts.FormattingEnabled = true;
+            this.cboProducts.Location = new System.Drawing.Point(193, 231);
+            this.cboProducts.Name = "cboProducts";
+            this.cboProducts.Size = new System.Drawing.Size(202, 29);
+            this.cboProducts.TabIndex = 0;
+            this.cboProducts.SelectedIndexChanged += new System.EventHandler(this.cboProducts_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 234);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Products";
+            // 
+            // lstSuppliers
+            // 
+            this.lstSuppliers.FormattingEnabled = true;
+            this.lstSuppliers.ItemHeight = 21;
+            this.lstSuppliers.Location = new System.Drawing.Point(561, 13);
+            this.lstSuppliers.Name = "lstSuppliers";
+            this.lstSuppliers.Size = new System.Drawing.Size(309, 529);
+            this.lstSuppliers.TabIndex = 2;
             // 
             // frmProductsSuppliers
             // 
@@ -38,18 +68,27 @@ namespace Group_1_Travel_Experts
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Group_1_Travel_Experts.Properties.Resources.MyBackground_;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(602, 398);
+            this.ClientSize = new System.Drawing.Size(1110, 568);
+            this.Controls.Add(this.lstSuppliers);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboProducts);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.Color.Maroon;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmProductsSuppliers";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Products + Suppliers";
+            this.Load += new System.EventHandler(this.frmProductsSuppliers_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cboProducts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstSuppliers;
     }
 }
--- Workshop 4/Group_1_Travel_Experts/frmProductsSuppliers.cs
+++ Workshop 4/Group_1_Travel_Experts/frmProductsSuppliers.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using Group_1_Travel_Experts.Models;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Text;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Group_1_Travel_Experts
@@ -13,6 +12,36 @@ namespace Group_1_Travel_Experts
         public frmProductsSuppliers()
         {
             InitializeComponent();
+        }
+
+        private void frmProductsSuppliers_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (TravelExpertsContext db = new TravelExpertsContext())
+                {
+                   cboProducts.DataSource = db.Products.OrderBy(p => p.ProductId).ToList();
+                   cboProducts.DisplayMember = "ProdName";
+                   cboProducts.ValueMember = "ProductId";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error when displaying data. {ex.Message}, {ex.GetType()}");
+            }
+        }
+
+        private void cboProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (TravelExpertsContext db = new TravelExpertsContext())
+            {
+                lstSuppliers.DataSource = db.ProductsSuppliers.Where(ps => ps.ProductId == cboProducts.SelectedIndex + 1)
+                                     .Join(db.Suppliers, 
+                                           ps => ps.SupplierId,
+                                           s => s.SupplierId,
+                                           (ps, s) => new { s.SupName }).ToList();
+                lstSuppliers.DisplayMember = "SupName";
+            }
         }
     }
 }
