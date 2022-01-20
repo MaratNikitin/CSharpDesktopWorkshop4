f25b8fd649bffdf44c7bf8bdbfd4813ba9a2d5a0
JackyLuong <jackyluong726@gmail.com>
2022-01-20 14:50:12 -0700

    Finished Product_Supplier Form
    - Changed from list box to DataGridView
    - Styled DataGridView
    - Joined Products, suppliers, and Product_Suppliers tables.
    - Get suppliers based on the product names selected in the combo box

--- Workshop 4/Group_1_Travel_Experts/frmProductsSuppliers.Designer.cs
+++ Workshop 4/Group_1_Travel_Experts/frmProductsSuppliers.Designer.cs
@@ -29,38 +29,81 @@ namespace Group_1_Travel_Experts
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmProductsSuppliers));
             this.cboProducts = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.lstSuppliers = new System.Windows.Forms.ListBox();
+            this.dgvSuppliers = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSuppliers)).BeginInit();
             this.SuspendLayout();
             // 
             // cboProducts
             // 
             this.cboProducts.FormattingEnabled = true;
-            this.cboProducts.Location = new System.Drawing.Point(193, 231);
+            this.cboProducts.Location = new System.Drawing.Point(95, 12);
             this.cboProducts.Name = "cboProducts";
-            this.cboProducts.Size = new System.Drawing.Size(202, 29);
+            this.cboProducts.Size = new System.Drawing.Size(235, 29);
             this.cboProducts.TabIndex = 0;
             this.cboProducts.SelectedIndexChanged += new System.EventHandler(this.cboProducts_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(98, 234);
+            this.label1.Location = new System.Drawing.Point(12, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 21);
             this.label1.TabIndex = 1;
             this.label1.Text = "Products";
             // 
-            // lstSuppliers
+            // dgvSuppliers
             // 
-            this.lstSuppliers.FormattingEnabled = true;
-            this.lstSuppliers.ItemHeight = 21;
-            this.lstSuppliers.Location = new System.Drawing.Point(561, 13);
-            this.lstSuppliers.Name = "lstSuppliers";
-            this.lstSuppliers.Size = new System.Drawing.Size(309, 529);
-            this.lstSuppliers.TabIndex = 2;
+            this.dgvSuppliers.AllowUserToOrderColumns = true;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Bisque;
+            this.dgvSuppliers.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvSuppliers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvSuppliers.BackgroundColor = System.Drawing.Color.Linen;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Bisque;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvSuppliers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvSuppliers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Linen;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Maroon;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvSuppliers.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvSuppliers.Location = new System.Drawing.Point(12, 62);
+            this.dgvSuppliers.Name = "dgvSuppliers";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Bisque;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvSuppliers.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvSuppliers.RowHeadersVisible = false;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.PowderBlue;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.Maroon;
+            this.dgvSuppliers.RowsDefaultCellStyle = dataGridViewCellStyle5;
+            this.dgvSuppliers.RowTemplate.Height = 25;
+            this.dgvSuppliers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvSuppliers.Size = new System.Drawing.Size(1086, 494);
+            this.dgvSuppliers.TabIndex = 9;
             // 
             // frmProductsSuppliers
             // 
@@ -69,7 +112,7 @@ namespace Group_1_Travel_Experts
             this.BackgroundImage = global::Group_1_Travel_Experts.Properties.Resources.MyBackground_;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1110, 568);
-            this.Controls.Add(this.lstSuppliers);
+            this.Controls.Add(this.dgvSuppliers);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cboProducts);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
@@ -80,6 +123,7 @@ namespace Group_1_Travel_Experts
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Products + Suppliers";
             this.Load += new System.EventHandler(this.frmProductsSuppliers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSuppliers)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,6 +133,6 @@ namespace Group_1_Travel_Experts
 
         private System.Windows.Forms.ComboBox cboProducts;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox lstSuppliers;
+        private System.Windows.Forms.DataGridView dgvSuppliers;
     }
 }
--- Workshop 4/Group_1_Travel_Experts/frmProductsSuppliers.cs
+++ Workshop 4/Group_1_Travel_Experts/frmProductsSuppliers.cs
@@ -9,6 +9,7 @@ namespace Group_1_Travel_Experts
 {
     public partial class frmProductsSuppliers : Form
     {
+        public Products selectedProd = null;
         public frmProductsSuppliers()
         {
             InitializeComponent();
@@ -20,9 +21,8 @@ namespace Group_1_Travel_Experts
             {
                 using (TravelExpertsContext db = new TravelExpertsContext())
                 {
-                   cboProducts.DataSource = db.Products.OrderBy(p => p.ProductId).ToList();
+                   cboProducts.DataSource = db.Products.OrderBy(p => p.ProdName).Select(p => p.ProdName).ToList();
                    cboProducts.DisplayMember = "ProdName";
-                   cboProducts.ValueMember = "ProductId";
                 }
             }
             catch (Exception ex)
@@ -35,13 +35,29 @@ namespace Group_1_Travel_Experts
         {
             using (TravelExpertsContext db = new TravelExpertsContext())
             {
-                lstSuppliers.DataSource = db.ProductsSuppliers.Where(ps => ps.ProductId == cboProducts.SelectedIndex + 1)
-                                     .Join(db.Suppliers, 
-                                           ps => ps.SupplierId,
-                                           s => s.SupplierId,
-                                           (ps, s) => new { s.SupName }).ToList();
-                lstSuppliers.DisplayMember = "SupName";
+
+                dgvSuppliers.DataSource = db.ProductsSuppliers
+                                     .Join(db.Suppliers, // join suppliers table
+                                           productSupplier => productSupplier.SupplierId, // Product_Supplier SupplierId
+                                           supplier => supplier.SupplierId, // Supplier SupplierId
+
+                                           // get all data from suppliers and Product_Supplier table
+                                           (productSupplier, supplier) => new { supplier, productSupplier })
+
+                                     .Join(db.Products, // join products table
+                                           newProductSupplier => newProductSupplier.productSupplier.ProductId, // Product_Supplier ProductId
+                                           product => product.ProductId, // Product ProductId
+
+                                           //get all data from Product_Suppliers and Products table
+                                           (newProductSupplier, product) => new { product, newProductSupplier })
+
+                                     //get all the suppliers that supplies the product with the same productName in the combo box
+                                     .Where(m => m.product.ProdName == cboProducts.SelectedValue.ToString())
+
+                                     .Select(m => new {m.newProductSupplier.supplier.SupName }).ToList(); // get supplier name
             }
+            dgvSuppliers.Columns["SupName"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill; // Fills the entire data grid view
+            dgvSuppliers.Columns["SupName"].HeaderText = "Suppliers";
         }
     }
 }
